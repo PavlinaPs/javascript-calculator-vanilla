24d0372c1b60bbb2e7cba0cd07dce57f63ce1e96
PPS <pavlina.psarsky@gmail.com>
2022-12-26 21:44:02 +0100

    done and working

---
+++ README.md
@@ -0,0 +1,61 @@
+## freeCodeCamp - Front End Development Libraries Project
+
+# Build a JavaScript Calculator (Vanilla)
+
+This is a vanilla JavaScript preparation for my React.js solution to the [Build a JavaScript Calculator]https://www.freecodecamp.org/learn/front-end-development-libraries/front-end-development-libraries-projects/build-a-javascript-calculator), the fourth of 5 projects to earn the Front End Development Libraries certification. FreeCodeCamp's mission is to help people learn to code for free.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Fulfill the user stories and get all of the tests to pass. You can use any mix of HTML, JavaScript, CSS, Bootstrap, SASS, React, Redux, and jQuery to complete this project.
+
+### Screenshot
+
+![Desktop layout](./screenshot.jpg)
+
+### Links
+
+- Solution URL: https://github.com/PavlinaPs/javascript-calculator-vanilla
+- Live Site URL: https://pavlinaps.github.io/javascript-calculator-vanilla/
+
+## My process
+
+I've already tried to code this challenge in React.js with useReducer hook but it never worked correctly, so I decided to code it vanilla first and it passes all FreeCodeCamp's tests. And also my husband's and children's fooling around :-)
+
+### Built with
+
+- vanilla JavaScript
+
+### What I learned
+
+This was a nice JavaScript exercise, let's see if I can code it in React.js
+
+### Continued development
+
+I need to improve in React.js.
+
+## Author
+
+- Website - [My portfolio](https://pavlinaps.github.io/my-portfolio/)
+- freeCodeCamp - [@pavlina1](https://www.freecodecamp.org/pavlina1)
+- Frontend Mentor - [@PavlinaPs](https://www.frontendmentor.io/profile/PavlinaPs)
+- Codewars - [PavlinaPs](https://www.codewars.com/users/PavlinaPs)
+- Twitter - [@PPsarsky](https://www.twitter.com/PPsarsky)
+
+## Acknowledgments
+
+It is great that I can learn to code with freeCodeCamp. I really appreciate what they are doing for the coding community. The projects are all very useful for me. All of them. Thank you!
--- calculator.js
+++ calculator.js
@@ -1,156 +1,176 @@
-// DOM selects
+//// DOM selects
 const expressionDisplay = document.getElementById("expression");
 const currentDisplay = document.getElementById("display");
 const buttons = Array.from(document.querySelectorAll("button"));
 
-// declare variables
+//// declare variables
 let expression = "";
 let currentOperand = "";
 let buttonClicked = "0";
 let overwrite = false;
 let operandAfterEval = "";
 
-// initial state
-currentDisplay.innerText = "0";
+//// initial display state
+display("", "0");
 
-// event listeners
-buttons.map((button) =>
-  button.addEventListener("click", () => {
-    buttonClicked = button.innerText;
-    if (buttonClicked === "CA") {
-      clearAll();
-      return;
-    }
-    if (buttonClicked === ".") {
-      if (currentOperand.indexOf(".") === -1) {
-        console.log("test1");
-        expression = `${expression}${buttonClicked}`;
-        currentOperand = `${currentOperand || ""}${buttonClicked}`;
-        expressionDisplay.innerText = expression;
-        currentDisplay.innerText = currentOperand;
-        return;
-      } else {
-        console.log("test2");
-        return;
-      }
-    }
-    //if (buttonClicked === "0" && currentOperand === "") {
-    if (buttonClicked === "0" && currentOperand === "0") {
-      return;
-    }
-    if (
-      expression.charAt(expression.length - 2).match(/[\-\+\*\/]/) &&
-      expression.charAt(expression.length - 1).match(/[\-\+\*\/]/) &&
-      buttonClicked.match(/[\+\*\/]/)
-    ) {
-      expression = `${expression.slice(0, -2)}${buttonClicked}`;
-      expressionDisplay.innerText = expression;
-      currentOperand = `${buttonClicked}`;
-      currentDisplay.innerText = currentOperand;
-      return;
-    }
-    if (
-      expression.charAt(expression.length - 1).match(/[\-]/) &&
-      buttonClicked.match(/[\+\*\/]/)
-    ) {
-      expression = `${expression.slice(0, -1)}${buttonClicked}`;
-      expressionDisplay.innerText = expression;
-      currentOperand = `${buttonClicked}`;
-      currentDisplay.innerText = currentOperand;
-      return;
-    }
-    if (
-      expression.charAt(expression.length - 1).match(/[\-\+\*\/]/) &&
-      buttonClicked.match(/[\-]/)
-    ) {
-      expression = `${expression}${buttonClicked}`;
-      expressionDisplay.innerText = expression;
-      currentOperand = `${buttonClicked}`;
-      currentDisplay.innerText = currentOperand;
-      return;
-    }
-    if (
-      expression.charAt(expression.length - 1).match(/[\+\*\/]/) &&
-      buttonClicked.match(/[\+\*\/]/)
-    ) {
-      expression = `${expression.slice(0, -1)}${buttonClicked}`;
-      expressionDisplay.innerText = expression;
-      currentOperand = `${buttonClicked}`;
-      currentDisplay.innerText = currentOperand;
-      return;
-    }
-    if (
-      buttonClicked.match(/[\-\+\*\/]/) &&
-      operandAfterEval !== "" &&
-      expression === ""
-    ) {
-      console.log(operandAfterEval);
-      expression = `${operandAfterEval}${buttonClicked}`;
-      currentOperand = `${operandAfterEval}${buttonClicked}`;
-      expressionDisplay.innerText = expression;
-      currentDisplay.innerText = currentOperand;
-      operandAfterEval = "";
-      return;
-    }
-    if (buttonClicked.match(/[\-\+\*\/]/)) {
-      completeOperand();
-      return;
-    }
-    if (
-      expression.charAt(expression.length - 1).match(/[\-\+\*\/]/) &&
-      buttonClicked === "="
-    ) {
-      return;
-    }
-    if (buttonClicked === "=") {
-      evaluate(expression);
-      return;
-    }
-    if (buttonClicked.match(/[\d]/) && !overwrite) {
-      expression = `${expression}${buttonClicked}`;
-      currentOperand = `${currentOperand || ""}${buttonClicked}`;
-      expressionDisplay.innerText = expression;
-      currentDisplay.innerText = currentOperand;
-      return;
-    }
-    if (buttonClicked.match(/[\d]/) && overwrite) {
-      expression = `${expression || ""}${buttonClicked}`;
-      currentOperand = `${buttonClicked}`;
-      expressionDisplay.innerText = expression;
-      currentDisplay.innerText = currentOperand;
-      overwrite = false;
-      return;
-    }
-    expression = `${expression || ""}${currentOperand}`;
-    expressionDisplay.innerText = expression;
-    currentDisplay.innerText = currentOperand;
-    overwrite = false;
+//// calculator logic
+function calculate(e) {
+  buttonClicked = e.target.innerText;
+
+  // clear all
+  if (buttonClicked === "CA") {
+    clearAll();
     return;
-  })
-);
+  }
+
+  // only one decimal allowed in an operand
+  if (buttonClicked === ".") {
+    if (currentOperand.indexOf(".") === -1) {
+      oneDecimal();
+      return;
+    } else {
+      return;
+    }
+  }
+
+  // multiple 0 at the beginning of an operand not allowed
+  if (buttonClicked === "0" && currentOperand === "0") {
+    return;
+  }
+
+  //// multiple consecutive operators entered
+  // 3rd operator entered
+  if (
+    expression.charAt(expression.length - 2).match(/[\-\+\*\/]/) &&
+    expression.charAt(expression.length - 1).match(/[\-\+\*\/]/) &&
+    buttonClicked.match(/[\+\*\/]/)
+  ) {
+    expression = `${expression.slice(0, -2)}${buttonClicked}`;
+    currentOperand = `${buttonClicked}`;
+    display(expression, currentOperand);
+    return;
+  }
+  // previous operator is -, clicked + or * or /
+  if (
+    expression.charAt(expression.length - 1).match(/[\-]/) &&
+    buttonClicked.match(/[\+\*\/]/)
+  ) {
+    expression = `${expression.slice(0, -1)}${buttonClicked}`;
+    currentOperand = `${buttonClicked}`;
+    display(expression, currentOperand);
+    return;
+  }
+  // entering negative number after an operator
+  if (
+    expression.charAt(expression.length - 1).match(/[\-\+\*\/]/) &&
+    buttonClicked.match(/[\-]/)
+  ) {
+    expression = `${expression}${buttonClicked}`;
+    currentOperand = `${buttonClicked}`;
+    display(expression, currentOperand);
+    return;
+  }
+  // consecutive operators other than -
+  if (
+    expression.charAt(expression.length - 1).match(/[\+\*\/]/) &&
+    buttonClicked.match(/[\+\*\/]/)
+  ) {
+    expression = `${expression.slice(0, -1)}${buttonClicked}`;
+    currentOperand = `${buttonClicked}`;
+    display(expression, currentOperand);
+    return;
+  }
+
+  // result is the first operand of a new operation
+  if (
+    buttonClicked.match(/[\-\+\*\/]/) &&
+    operandAfterEval !== "" &&
+    expression === ""
+  ) {
+    resultIsFirstOperand();
+    return;
+  }
+
+  // to complete an operand
+  if (buttonClicked.match(/[\-\+\*\/]/)) {
+    completeOperand();
+    return;
+  }
+
+  // if "=" is clicked after an operator
+  if (
+    expression.charAt(expression.length - 1).match(/[\-\+\*\/]/) &&
+    buttonClicked === "="
+  ) {
+    return;
+  }
+
+  // evaluation after "="
+  if (buttonClicked === "=") {
+    evaluate(expression);
+    return;
+  }
+
+  // adds another digit to an operand, overwrite flag is off
+  if (buttonClicked.match(/[\d]/) && !overwrite) {
+    addAnotherDigit();
+    return;
+  }
+
+  // starts a new operand, overwrite flag is on
+  if (buttonClicked.match(/[\d]/) && overwrite) {
+    startNewOperand();
+    return;
+  }
+
+  expression = `${expression || ""}${currentOperand}`;
+  overwrite = false;
+  display(expression, currentOperand);
+  return;
+}
+
+//// helper functions
+// to display expression and current operand/operator
+function display(displayExpression, displayCurrent) {
+  expressionDisplay.innerText = displayExpression;
+  currentDisplay.innerText = displayCurrent;
+}
 
 function clearAll() {
   expression = "";
   buttonClicked = "0";
   currentOperand = "";
   operandAfterEval = "";
-  expressionDisplay.innerText = "";
-  currentDisplay.innerText = "0";
+  display("", "0");
+}
+
+function oneDecimal() {
+  expression = `${expression}${buttonClicked}`;
+  currentOperand = `${currentOperand || ""}${buttonClicked}`;
+  display(expression, currentOperand);
+}
+
+function resultIsFirstOperand() {
+  expression = `${operandAfterEval}${buttonClicked}`;
+  currentOperand = `${operandAfterEval}${buttonClicked}`;
+  operandAfterEval = "";
+  display(expression, currentOperand);
 }
 
 function completeOperand() {
   expression = `${expression}${buttonClicked}`;
-  currentDisplay.innerText = buttonClicked;
-  expressionDisplay.innerText = expression;
+  display(expression, buttonClicked);
   overwrite = true;
 }
 
 function evaluate(string) {
+  // to fix the issue of two consecutive minuses
   if (/(--)/.test(string)) {
-    let newString = string.replace(/--/, "+");
-    string = newString;
+    let editedString = string.replace(/--/, "+");
+    string = editedString;
   }
   let result = parseFloat(eval(string));
+  // to compensate for JavaScript floats' computation issue
   let resultRounded = (
     Math.round(1000000000000 * result) / 1000000000000
   ).toString();
@@ -160,3 +180,19 @@ function evaluate(string) {
   expression = "";
   operandAfterEval = resultRounded;
 }
+
+function addAnotherDigit() {
+  expression = `${expression}${buttonClicked}`;
+  currentOperand = `${currentOperand || ""}${buttonClicked}`;
+  display(expression, currentOperand);
+}
+
+function startNewOperand() {
+  expression = `${expression || ""}${buttonClicked}`;
+  currentOperand = `${buttonClicked}`;
+  overwrite = false;
+  display(expression, currentOperand);
+}
+
+//// event listeners
+buttons.map((button) => button.addEventListener("click", calculate));
